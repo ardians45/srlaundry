6ab739891fbfa081a44990755125aa870a40c496
ardians45 <ardians1945@gmail.com>
2026-05-05 11:08:08 +0700

    chore: update site verification, domain config, and SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://srlaundry.vercel.app',
+  site: 'https://www.srlaundry.site',
   build: {
     inlineStylesheets: 'always'
   },
